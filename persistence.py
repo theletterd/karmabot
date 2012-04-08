3f33e7d11e54fd0b1daa2885548a9cf19219a4b6
Duncan Cook <duncan.cook@theletterd.co.uk>
2012-04-08 09:56:59 -0700

    fix silly karma bug

--- persistence.py
+++ persistence.py
@@ -65,7 +65,7 @@ class Persistence(object):
             increments = (count + total_score) / 2
             decrements = increments - total_score
 
-            return "%s has karma of %s (+%d/-%d)" % (username, count, increments, decrements)
+            return "%s has karma of %s (+%d/-%d)" % (username, total_score, increments, decrements)
         else:
             cursor.execute('select name, sum(score) as sum_score from %s group by name order by sum_score desc limit 3' % KARMA_TABLE)
             return  "Most karmic: " + ', '.join("%s (%s)" % entry for entry in cursor)
