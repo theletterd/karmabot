bad2ccf21de6ee0794a135995667f6f53b2ee6f5
Duncan Cook <duncan.cook@theletterd.co.uk>
2012-06-19 15:41:34 +0530

    http derp

--- markov.py
+++ markov.py
@@ -6,7 +6,7 @@ import random
 import re
 
 STOP_WORD = "\n"
-URL_REGEX = '(https?://)?(www.*\.)|([a-z0-9.\-]+[\.][a-z]{2,4}/)'
+URL_REGEX = '(https?://)|(www.*\.)|([a-z0-9.\-]+[\.][a-z]{2,4}/)'
 # from http://daringfireball.net/2010/07/improved_regex_for_matching_urls, not working properly yet.
 # URL_REGEX_2 = """(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))"""
 
